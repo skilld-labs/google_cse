27087439e63949384c6128eec06d94467898bf80
Valeriy Kozhevnikov <i271980@gmail.com>
2016-06-03 01:57:13 +0300

    update JS code

--- js/google_cse.js
+++ js/google_cse.js
@@ -1,32 +1,37 @@
 (function ($, Drupal, drupalSettings) {
   Drupal.behaviors.googleCSECustomSearch = {
     attach: function (context, settings) {
-      var googleCSEWatermark = function (id) {
-        var f = $(id)[0];
-        if (f && (f.query || f['edit-search-block-form--2'] || f['edit-keys'] || f['[data-drupal-selector="search-block-form"]'])) {
-          var q = f.query ? f.query : (f['edit-search-block-form--2'] ? f['edit-search-block-form--2'] : f['edit-keys']);
-          var n = navigator;
-          var l = location;
-          if (n.platform == 'Win32') {
-            q.style.cssText = 'border: 1px solid #7e9db9; padding: 2px;';
-          }
-          var b = function () {
-            if (q.value == '') {
-              q.style.background = '#FFFFFF url(https://www.google.com/cse/intl/' + drupalSettings.googleCSE.language + '/images/google_custom_search_watermark.gif) left no-repeat';
-            }
-          };
-          var f = function () {
-            q.style.background = '#ffffff';
-          };
-          q.onfocus = f;
-          q.onblur = b;
-          b();
-        }
+      var getWatermarkBackground = function(value) {
+        var googleCSEBaseUrl = 'https://www.google.com/cse/intl/';
+        var googleCSEImageUrl = 'images/google_custom_search_watermark.gif';
+        var language = drupalSettings.googleCSE.language ? '' : drupalSettings.googleCSE.language + '/';
+        return value ? '' : ' url(' + googleCSEBaseUrl + language + googleCSEImageUrl + ') left no-repeat';
+      }
+      var onFocus = function(e) {
+        $(e.target).css('background', '#ffffff');
       };
+      var onBlur = function(e) {
+        $(e.target).css('background', '#ffffff' + getWatermarkBackground($(e.target).val()));
+      };
+      var googleCSEWatermark = function (selector) {
+        var form = jQuery(selector);
+        var searchInputs = $('[data-drupal-selector="edit-keys"]', form);
+        if (navigator.platform == 'Win32') {
+          searchInputs.css('style', 'border: 1px solid #7e9db9; padding: 2px;');
+        }
+
+        searchInputs.blur(onBlur);
+        searchInputs.focus(onFocus);
+        searchInputs.each(function() {
+          var event = new Object();
+          event.target = this;
+          onBlur(event);
+        });
+      };
+
+      googleCSEWatermark('[data-drupal-selector="search-block-form"] [data-drupal-form-fields="edit-keys--2"]');
       googleCSEWatermark('[data-drupal-selector="search-block-form"] [data-drupal-form-fields="edit-keys"]');
-      googleCSEWatermark('.search-block-form.google-cse');
-      googleCSEWatermark('#search-form.google-cse');
-      googleCSEWatermark('#google-cse-results-searchbox-form');
+      googleCSEWatermark('[data-drupal-selector="search-form"]');
     }
   };
 })(jQuery, Drupal, drupalSettings);
